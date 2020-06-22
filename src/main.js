0e2991e04ef5cf07ec2529c5148fb9a4950174f5
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-22 15:55:16 -0700

    user interface
    co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import './styles.css';
 import { Entry } from './journal.js';
+import $ from 'jquery';
 
-
-const entry = new Entry ("title", "body");
+$(document).ready(function() {
+  $("form").submit(function() {
+    event.preventDefault();
+    let inputtedTitle = $("#title").val();
+    let inputtedBody = $("#body").val();
+    let entry = new Entry(inputtedTitle, inputtedBody);
+    let journalEntry = "<li><h4>" + entry.title + "</h4><p>" + entry.body + "</p><p>Words: " + entry.numberOfWordsInBody() + "; Vowels: " + entry.numberOfVowels() + "; Consonants: " + entry.numberOfConsonants() + "</p></li>";
+    $("ol").append(journalEntry);
+  });
+});
